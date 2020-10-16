3b3409592c1306d61b03bcf04b8eb2f4f237bf10
JoroPaspalev <50412345+JoroPaspalev@users.noreply.github.com>
2020-10-16 21:07:00 +0300

    Add SHA512 and SHA256 Hash Algorithms

---
+++ SHA256_Hash_Agorithm/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace SHA256_Hash_Agorithm
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string result = Sha256("feelgood");
+            Console.WriteLine(result); //bafedefe815af32189f08f31f4dcfaa05c86c201a85b556f9b74496869fb67ff
+            Console.WriteLine(result.Length);//64
+        }
+
+        public static string Sha256(string text)
+        {
+            byte[] bytes = Encoding.Unicode.GetBytes(text);
+            SHA256Managed hashstring = new SHA256Managed();
+            byte[] hash = hashstring.ComputeHash(bytes);
+            string hashString = string.Empty;
+            foreach (byte x in hash)
+            {
+                hashString += String.Format("{0:x2}", x);
+            }
+            return hashString;
+        }
+    }
+}
--- Sha512_Hash_Algorithm/Program.cs
+++ Sha512_Hash_Algorithm/Program.cs
@@ -11,6 +11,7 @@ namespace Sha512_Hash_Algorithm
             //DA7A634D69ED8EABB1FC8116877EF3C61D68630F0A7B3C04AEB389977272306E5B86A0EBDE8F7F8D4A4DE0545D28CE76B6CB374725F415E3ADC1B2163146EA92
 
             Console.WriteLine(hashedPass);
+            Console.WriteLine(hashedPass.Length);
         }
 
         public static string SHA512(string input)
